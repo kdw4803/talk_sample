f4f4f4f6815e90279543715a52563a8c60abcb01
Brad <brad@Bradui-MacBook-Pro.local>
2017-04-02 21:17:32 +0900

    add login

--- talk_sample/AuthViewController.swift
+++ talk_sample/AuthViewController.swift
@@ -14,13 +14,22 @@ class AuthViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    var isLogin: Bool?
     
     @IBAction func buttonPressed(_ sender: Any) {
-        FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: {_,error in
-            if (error == nil) {
-            self.performSegue(withIdentifier: "ToMain", sender: sender)
-            }
-        })
+        if isLogin! {
+            FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: {_,error in
+                if (error == nil) {
+                    self.performSegue(withIdentifier: "ToMain", sender: sender)
+                }
+            })
+        } else {
+            FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: {_,error in
+                if (error == nil) {
+                    self.performSegue(withIdentifier: "ToMain", sender: sender)
+                }
+            })
+        }
     }
 
     override func viewDidLoad() {
